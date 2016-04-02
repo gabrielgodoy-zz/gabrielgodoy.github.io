d9baa217e0bdc30851cc5498e3aaa48fe5f408e3
Gabriel Godoy <ggodoyoliveira@gmail.com>
2016-04-02 02:01:40 -0300

    Add js task | headers | Minimist

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,41 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     pl = require('gulp-load-plugins')(),
-    cp = require('child_process');
+    cp = require('child_process'),
+    env = require('minimist')(process.argv.slice(2));
 
 function jumpError (error) {
   console.log(error.toString());
   this.emit('end');
 }
 
-gulp.task('watch', function() {
-    gulp.watch('assets/_styl/**/*.styl', ['stylus']);
-    gulp.watch('assets/js/**/*.js', ['js']);
-    gulp.watch([
-        '**/*.html',
-        '**/*.md',
-        'index.html',
-        '_includes/*.html',
-        '_layouts/*.html',
-        '_posts/*'
-    ], ['jekyll-rebuild']);
-});
-
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> jekyll build'
 };
 
-// Style
-gulp.task('stylus', function() {
+/* =============================================================
+Rebuild Jekyll & do page reload
+*/
+gulp.task('jekyll-rebuild', ['jekyll-build'], () => {
+    browserSync.reload();
+});
+
+/* =============================================================
+Browser Sync
+*/
+gulp.task('browser-sync', ['jekyll-build'], () => {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        notify: false
+    });
+});
+
+/* =============================================================
+Style
+*/
+gulp.task('stylus', () => {
     return gulp.src('./assets/_styl/main.styl')
         .pipe(pl.sourcemaps.init())
         .pipe(pl.stylus({
@@ -45,8 +54,37 @@ gulp.task('stylus', function() {
         .pipe(gulp.dest('./assets/css/'));
 });
 
-// Build the Jekyll Site
-gulp.task('jekyll-build', function(done) {
+/* =============================================================
+Javascript
+*/
+gulp.task('js', () => {
+    return gulp.src((env.prod) ? ['assets/js/**/*.js', '!assets/js/main.js'] : ['assets/js/**/*.js', '!assets/js/analytics.js', '!assets/js/main.js'])
+        .on('error', jumpError)
+        .pipe(pl.concat('main.js'))
+        .pipe(pl.uglify())
+        .pipe(gulp.dest('assets/js/'));
+});
+
+/* =============================================================
+Watcher
+*/
+gulp.task('watch', () =>  {
+    gulp.watch('assets/_styl/**/*.styl', ['stylus']);
+    gulp.watch('assets/js/**/*.js', ['js']);
+    gulp.watch([
+        '**/*.html',
+        '**/*.md',
+        'index.html',
+        '_includes/*.html',
+        '_layouts/*.html',
+        '_posts/*'
+    ], ['jekyll-rebuild']);
+});
+
+/* =============================================================
+Build the Jekyll Site
+*/
+gulp.task('jekyll-build', (done) => {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {
             stdio: 'inherit'
@@ -54,18 +92,4 @@ gulp.task('jekyll-build', function(done) {
         .on('close', done);
 });
 
-// Rebuild Jekyll & do page reload
-gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
-    browserSync.reload();
-});
-
-// Browser Sync
-gulp.task('browser-sync', ['jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
-
-gulp.task('default', ['stylus', 'browser-sync', 'watch']);
+gulp.task('default', ['stylus', 'js', 'browser-sync', 'watch']);
