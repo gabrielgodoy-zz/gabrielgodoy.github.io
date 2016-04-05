03a48e24408d94ca0c04b4f61de5f46af4f98457
Gabriel Godoy <ggodoyoliveira@gmail.com>
2016-04-05 02:07:25 -0300

    Add scroll behaviour

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     'use strict';
     if (document.getElementsByClassName('single-post')[0]) {
         var shareBts = document.getElementsByClassName('share-bts')[0];
-        var singlePosts = document.getElementsByClassName('single-post')[0];
+        var aboutAuthor = document.getElementsByClassName('about-author')[0];
+
+        // Get elements positions relative to the top of the viewport
+        var aboutAuthorTop = window.scrollY + aboutAuthor.getBoundingClientRect().top;
+        var shareBtTop = window.scrollY + shareBts.getBoundingClientRect().top;
 
         window.addEventListener('scroll', function() {
-            if (window.scrollY - 20 > singlePosts.offsetTop) {
-                singlePosts.classList.add('single-post--leftmargin');
-                shareBts.classList.add('share-bts--fixed');
+            if (window.scrollY > (aboutAuthorTop - shareBts.offsetHeight - 130)) {
+                return false;
+            } else if ((window.scrollY + 58) > shareBtTop) {
+                shareBts.style.top = ((window.scrollY - shareBtTop) + shareBts.offsetHeight + 50) + 'px';
             } else {
-                singlePosts.classList.remove('single-post--leftmargin');
-                shareBts.classList.remove('share-bts--fixed');
+                shareBts.style.top = '85px';
             }
         });
     }
